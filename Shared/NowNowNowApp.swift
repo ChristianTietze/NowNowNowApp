6ebd220e9953808cffcf5fa9a1f1ebe0a89b91de
Christian Tietze <me@christiantietze.de>
2021-06-18 21:51:02 +0200

    Initial Commit

---
+++ Shared/NowNowNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowNowNowApp.swift
+//  Shared
+//
+//  Created by Christian Tietze on 18.06.21.
+//
+
+import SwiftUI
+
+@main
+struct NowNowNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
