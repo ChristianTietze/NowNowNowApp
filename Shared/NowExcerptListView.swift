4965fcdb7ad6256cb9f4d4c3bff0524b543c07ff
Christian Tietze <me@christiantietze.de>
2021-06-30 10:24:55 +0200

    extract excerpt production into NowExcerptPResenter

--- Shared/NowExcerptListView.swift
+++ Shared/NowExcerptListView.swift
@@ -3,7 +3,7 @@
 import SwiftUI
 
 struct NowExcerptListView: View {
-    var excerpts: [NowExcerptViewModel]
+    @Binding var excerpts: [NowExcerptViewModel]
 
     var body: some View {
         List(excerpts, id: \.id) { excerpt in
@@ -15,9 +15,9 @@ struct NowExcerptListView: View {
 
 struct NowExcerptListView_Previews: PreviewProvider {
     static var previews: some View {
-        NowExcerptListView(excerpts: [
+        NowExcerptListView(excerpts: .constant([
             NowExcerptViewModel(id: UUID(), title: "Test", updatedAt: "2021-06-18", excerpt: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", icon: .nowPlaceholderIcon),
             NowExcerptViewModel(id: UUID(), title: "Test 2", updatedAt: "2021-06-19", excerpt: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum", icon: .nowPlaceholderIcon),
-        ])
+        ]))
     }
 }
--- Shared/NowNowNowApp.swift
+++ Shared/NowNowNowApp.swift
@@ -4,31 +4,11 @@ import SwiftUI
 
 @main
 struct NowNowNowApp: App {
-    let dateFormatter: DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.timeStyle = .short
-        dateFormatter.dateStyle = .short
-        return dateFormatter
-    }()
+    @StateObject var excerptPresenter: NowExcerptPresenter = NowExcerptPresenter()
 
     var body: some Scene {
         WindowGroup {
-            let urls: [URL] = [
-                URL(string: "https://mattgemmell.com/now")!,
-                URL(string: "https://sivers.org/now")!,
-                URL(string: "https://tyler.io/now/")!,
-                URL(string: "https://denisdefreyne.com/now/")!,
-                URL(string: "https://christiantietze.de/now")!
-            ]
-            let excerpts = urls.map { url -> NowExcerptViewModel in
-                NowExcerptViewModel(
-                    id: UUID(),
-                    title: url.absoluteString,
-                    updatedAt: dateFormatter.string(from: Date()),
-                    excerpt: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    icon: .nowPlaceholderIcon)
-            }
-            NowExcerptListView(excerpts: excerpts)
+            NowExcerptListView(excerpts: $excerptPresenter.excerpts)
         }
     }
 }
