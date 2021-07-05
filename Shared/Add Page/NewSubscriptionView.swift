5536c720b853c7305fc0444532d08935d16730bd
Christian Tietze <me@christiantietze.de>
2021-07-05 11:27:11 +0200

    perform HTTP HEAD request to validate URLs

--- Shared/Add Page/NewSubscriptionView.swift
+++ Shared/Add Page/NewSubscriptionView.swift
@@ -61,30 +61,68 @@ struct NewSubscriptionView: View {
 import Combine
 
 class NowPageURLValidator: ObservableObject {
+    @Published var isPerformingNetworkActivity: Bool = false
     @Published var text: String = ""
+    @Published var validURL: URL? = nil
 
-    var potentiallyValidURL: AnyPublisher<URL?, Never> {
+    init() {
+        let networkActivityPublisher = PassthroughSubject<Bool, Never>()
+
+        networkActivityPublisher
+            .receive(on: RunLoop.main)
+            .assign(to: &$isPerformingNetworkActivity)
+
         $text
-            .removeDuplicates()
             .debounce(for: 0.2, scheduler: RunLoop.main)
-            .map(URL.init(string:))
-            .map { ($0?.isPotentiallyValid() ?? false) ? $0 : nil }
+            .removeDuplicates()
+            .compactMap { URL.reachableURL(string: $0, networkActivityPublisher: networkActivityPublisher) }
+            .switchToLatest()
+            .receive(on: RunLoop.main)
+            .assign(to: &$validURL)
+    }
+}
+
+
+extension URL {
+    /// - Returns: Promise of the URL result.
+    static func reachableURL(string: String, networkActivityPublisher: PassthroughSubject<Bool, Never>) -> AnyPublisher<URL?, Never> {
+        print(string)
+        guard let url = URL(string: string)
+        else { return Just(nil).eraseToAnyPublisher() }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "HEAD"  // Just perform a check, don't fetch content
+
+        return URLSession.shared
+            .dataTaskPublisher(for: request)
+            .handleEvents(receiveSubscription: { _ in
+                networkActivityPublisher.send(true)
+            }, receiveCompletion: { _ in
+                networkActivityPublisher.send(false)
+            }, receiveCancel: {
+                networkActivityPublisher.send(false)
+            })
+            .tryMap { data, response -> URL? in
+                guard let urlResponse = response as? HTTPURLResponse,
+                      urlResponse.isStatusIndicatedReachability
+                else { return nil }
+                return urlResponse.url?.absoluteURL
+            }
+            .catch { err in
+                return Just(nil)
+            }
             .eraseToAnyPublisher()
     }
 }
 
-extension URL {
-    /// Tests URL for reachability based on `NSURLConnection.canHandle`.
-    fileprivate func isPotentiallyValid() -> Bool {
-        let request = URLRequest(url: self)
-        let canHandle = NSURLConnection.canHandle(request)
-        return canHandle
+extension HTTPURLResponse {
+    var isStatusIndicatedReachability: Bool {
+        (200 ..< 400).contains(statusCode) || statusCode == 405
     }
 }
 
 struct NowPageURLView: View {
     @StateObject var urlValidator = NowPageURLValidator()
-    @State private var validURL: URL?
 
     var body: some View {
         urlTextField
@@ -103,8 +141,10 @@ struct NowPageURLView: View {
                     .frame(minWidth: 250)
             }
 
-            Text("\(validURL?.absoluteString ?? "")")
-                .onReceive(urlValidator.potentiallyValidURL) { validURL = $0 }
+            Text(urlValidator.isPerformingNetworkActivity ? "Active" : "-")
+
+            Text(urlValidator.validURL.map(\.absoluteString) ?? "(none)")
+
         }
     }
 }
