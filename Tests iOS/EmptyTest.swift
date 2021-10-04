a1e07317bddbc60e6ec11ddac939fb891126d96d
Christian Tietze <me@christiantietze.de>
2021-10-04 11:41:24 +0200

    add empty UI test to make iOS UI tests compile

---
+++ Tests iOS/EmptyTest.swift
@@ -0,0 +1,9 @@
+//  Copyright © 2021 Christian Tietze. All rights reserved. Distributed under the MIT License.
+
+import XCTest
+
+class EmptyTest: XCTestCase {
+    func testExample() throws {
+        XCTFail("UI Tests not implemented; file added to have non-empty build product")
+    }
+}
