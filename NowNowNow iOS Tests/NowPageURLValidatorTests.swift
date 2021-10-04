199b26024709f3190cf0cc214c26a74f471aff60
Christian Tietze <me@christiantietze.de>
2021-10-04 11:41:57 +0200

    prepare unit tests for iOS

---
+++ NowNowNow iOS Tests/NowPageURLValidatorTests.swift
@@ -0,0 +1,9 @@
+//  Copyright © 2021 Christian Tietze. All rights reserved. Distributed under the MIT License.
+
+import XCTest
+import Combine
+import Foundation
+@testable import NowNowNow
+
+class NowPageURLValidatorTests: XCTestCase {
+}
